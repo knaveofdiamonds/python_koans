01e16161ef754a3f8f8fdabae4681f1a72e9a52f
Roland Swingler <roland.swingler@gmail.com>
2015-04-27 10:28:04 +0100

    Triangle Exception Koan.

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -18,10 +18,15 @@
 #   about_triangle_project_2.py
 #
 def triangle(a,b,c):
-    sides = len(set([a,b,c]))
-    if sides == 1:
+    sides = sorted([a,b,c])
+
+    if sides[0] + sides[1] <= sides[2]:
+        raise TriangleError, "These are not sides of a triangle"
+
+    distinct_sides = len(set(sides))
+    if distinct_sides == 1:
         return 'equilateral'
-    elif sides == 2:
+    elif distinct_sides == 2:
         return 'isosceles'
     else:
         return 'scalene'
